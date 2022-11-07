b8eba6b372807c3b65dada975449886ec9a34c90
Briana Zamora <briana.z@hotmail.com>
2022-11-07 08:16:11 -0800

    adding homework

---
+++ src/week1homeworklabs/week1lab1.java
@@ -0,0 +1,31 @@
+package week1homeworklabs;
+
+public class week1lab1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// create a variable to hold the quantity of available seats left on a plane
+		int availableFlightSeats = 20;
+		// create a variable to hold the cost of groceries at checkout
+		double costOfGroceries = 34.75;
+		//create a variable to hold a person's middle initial
+		char middleInitial = 'A';
+		//create a variable to hold true if it's hot outside and false if it's cold outside
+		boolean isHotOutside = true;
+		//create a variable to hold a customer's first name
+		String firstName = "Armani";
+		//create a variable to hold a street address
+		String streetAddress = "344 W Arrowbird";
+		//print all variables to console
+		System.out.println("Available seats left on plane: " + availableFlightSeats);
+		System.out.println(costOfGroceries + "is the cost of groceries");
+		System.out.println(middleInitial);
+		System.out.println(isHotOutside);
+		System.out.println(firstName);
+		System.out.println(streetAddress); }
+		
+		
+		
+	
+
+}
